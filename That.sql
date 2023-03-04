718aaf31fa0dd78562a9c9971fc948278b2f6eb6
Alexander Beers <abeers@cs506-team-31.cs.wisc.edu>
2023-03-04 17:29:45 -0600

    this java

---
+++ That.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `ThisAndThat`.`That` (
+	`That_pk` INT NOT NULL auto_increment,
+	`That_mno` VARCHAR(45) NULL,
+	`That_pqr` VARCHAR(45) NULL,
+	`That_This_pk` INT NULL,
+	PRIMARY KEY (`That_pk`));
