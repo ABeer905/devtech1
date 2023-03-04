bd863ee2d39c4078bfa4a5026d5dbd29febd732a
ab <arbeers@wisc.edu>
2023-03-04 17:49:26 -0600

    Thisandthatjdbc

---
+++ ThisAndThatJDBC.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+class MysqlCon {
+	public static void main(String args[]) {
+		try{
+			Connection dbCxn=DriverManager.getConnection(
+			  "jdbc:mysql://localhost:43306/ThisAndThat","root","password"
+			 );
+			Statement thatStmt=dbCxn.createStatement();
+			ResultSet rsThat=thatStmt.executeQuery("select * from That");
+			while(rsThat.next()) {
+				int foreignKeyToThis = rsThat.getInt(4);
+				System.out.println(rsThat.getInt(1)
+					+ " " + rsThat.getString(2)
+					+ " " + rsThat.getString(3)
+					+ " "  + rsThat.getInt(4));
+
+				String thisQuery = 
+					"select * from This where This_pk = '" + foreignKeyToThis + "'";
+				System.out.println(thisQuery);
+				Statement thisStmt=dbCxn.createStatement();
+				ResultSet rsThis=thisStmt.executeQuery(thisQuery);
+
+				while(rsThis.next()){
+					System.out.println("\t" + rsThis.getInt(1)
+						+ " " + rsThis.getString(2)
+						+ " " + rsThis.getInt(3)
+						+ " " + rsThis.getDate(4)
+						+ " " + rsThis.getString(5));
+				}
+			}
+			dbCxn.close();
+		}catch(Exception e) { System.out.println(e);}
+	}
+}
